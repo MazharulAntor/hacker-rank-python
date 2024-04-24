150198b7ae3aead169bd785878699a699c5c97df
Md Mazharul Islam Antor <mazharul.antor@htbbd.com>
2024-04-24 06:47:53 +0600

    HR12

--- HR12.py
+++ HR12.py
@@ -1,7 +1,36 @@
 if __name__ == '__main__':
     N = int(input())
 
+    numbers = []
     command = ""
+    position = None
+    value = None
+    printList = []
     for _ in range(N):
         command = input()
+        if command.startswith("insert"):
+            _, position, value = command.split()
+            position = int(position)
+            value = int(value)
+            numbers.insert(position, value)
+        elif command.startswith("print"):
+            printList.append(numbers.copy())
+        elif command.startswith("remove"):
+            _, value = command.split()
+            value = int(value)
+            numbers.remove(value)
+        elif command.startswith("append"):
+            _, value = command.split()
+            value = int(value)
+            numbers.append(value)
+        elif command.startswith("sort"):
+            numbers.sort()
+        elif command.startswith("pop"):
+            numbers.pop()
+        elif command.startswith("reverse"):
+            numbers.reverse()
+        else:
+            pass
 
+    for number in printList:
+        print(number)
