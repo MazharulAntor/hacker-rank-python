3d8bd8a01a4192c9a6072baa271a6566b7d4b4bd
Mazharul Islam Antor <mazharulantor2015@gmail.com>
2024-04-29 16:11:41 +0600

    Merge the Tools!

--- HR27.py
+++ HR27.py
@@ -1,13 +1,16 @@
 def merge_the_tools(string, k):
     no_of_substrings = int(len(string) / k)
     counter = 0
+    pos = k
     for i in range(no_of_substrings):
-        substring = string[counter:k]
-        counter = k
-        k += k
+        substring = string[counter:pos]
+        counter = pos
+        pos += k
+        final_substring = ""
         for j in range(len(substring)):
-
-            print(substring)
+            if not substring[j] in final_substring:
+                final_substring += substring[j]
+        print(final_substring)
 
 
 if __name__ == '__main__':
