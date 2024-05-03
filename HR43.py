e7014a938225ca425926316bdcd8ef52f79b8eb2
Md Mazharul Islam Antor <mazharulantor2015@gmail.com>
2024-05-03 10:03:40 +0600

    Set .symmetric_difference()Operation

---
+++ HR43.py
@@ -0,0 +1,7 @@
+eng_sub = int(input())
+eng_rolls = set(map(int, input().split()))
+fr_sub = int(input())
+fr_rolls = set(map(int, input().split()))
+
+roll_students = eng_rolls ^ fr_rolls
+print(len(roll_students))
