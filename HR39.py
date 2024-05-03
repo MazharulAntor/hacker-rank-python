11f0629e7a343316b905e2af8b95575d5fc2eac7
Md Mazharul Islam Antor <mazharulantor2015@gmail.com>
2024-05-03 09:21:24 +0600

    Set .discard(), .remove() & .pop()

--- HR39.py
+++ HR39.py
@@ -1,3 +1,14 @@
 n = int(input())
 s = set(map(int, input().split()))
+N = int(input())
 
+for _ in range(N):
+    command = input()
+    if command.startswith("remove"):
+        if int(command[7:]) in s:
+            s.remove(int(command[7:]))
+    if command.startswith("discard"):
+        s.discard(int(command[8:]))
+    if command.startswith("pop"):
+        s.pop()
+print(sum(s))
